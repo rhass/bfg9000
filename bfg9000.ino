b9e25ff2c1097dc4f6d5c8aae1f62646b025f8f7
Ryan Hass <ryan@invalidchecksum.net>
2013-09-15 19:15:49 -0700

    Refactor project for Energia/MSP430G2553. Commented out Serial interactions in attempt to debug hardware specific issues with this port.

--- bfg9000.ino
+++ bfg9000.ino
@@ -1,4 +1,4 @@
-// Arduino built-in i2c library.
+// i2c library.
 #include <Wire.h>
 // LCD Libraries
 #include <ST7036.h>
@@ -17,34 +17,33 @@
 #define PRESSURE_MAX 150   // 150psi
 
 // Application/implementation specific pin configuration.
-#define ONBOARD_LED   13
-#define TRIGGER_INPUT 2
-#define TRIGGER_RELAY 4
-#define SAFETY_INPUT  8
-#define PUMP_RELAY    7
+//#define ONBOARD_LED   P1_0
+#define TRIGGER_INPUT P1_3
+#define TRIGGER_RELAY P1_4
+#define SAFETY_INPUT  P2_6
+#define PUMP_RELAY    P1_1
 // Tank pressure settings -- the pump cannot output more than 20 PSI.
 #define MIN_TANK_PSI  10.0
 #define MAX_TANK_PSI  20.0
 
 void setup() {
-  pinMode(ONBOARD_LED, OUTPUT);
-  pinMode(TRIGGER_INPUT, INPUT);
+  //pinMode(ONBOARD_LED, OUTPUT);
+  pinMode(TRIGGER_INPUT, INPUT_PULLUP);
   pinMode(TRIGGER_RELAY, OUTPUT);
   pinMode(SAFETY_INPUT, INPUT);
   pinMode(PUMP_RELAY, OUTPUT);
-
-  digitalWrite(TRIGGER_INPUT, LOW);
-  Serial.begin(115200);
+  Serial.begin(9600);
 }
 
 void fire() {
   if ( safety_engaged() == false ) {
-    Serial.println(F("FIRE!!!"));
+    Serial.println("FIRE!!!");
     digitalWrite(TRIGGER_RELAY, HIGH);
     delay(25);
     digitalWrite(TRIGGER_RELAY, LOW);
-  } else {
-    Serial.println(F("Safety is engadged."));
+  } 
+  else {
+    Serial.println("Safety is engadged.");
   }
 }
 
@@ -53,32 +52,32 @@ float currentPsi() {
   uint8_t error_level = ps_get_raw(SLAVE_ADDR, &ps);
   float psi, therm;
   if ( error_level == 4 ) {
-    Serial.println(F("err pressure sensor missing"));
+    Serial.println("err pressure sensor missing");
   } 
   else {
     if ( error_level == 3 ) {
-      Serial.print(F("err pressure sensor diagnostic fault "));
+      Serial.print("err pressure sensor diagnostic fault ");
       Serial.println(ps.status, BIN);
     }
     if ( error_level == 2 ) {
       // if data has already been feched since the last
       // measurement cycle
-      Serial.print(F("warn stale pressure data "));
+      Serial.print("warn stale pressure data ");
       Serial.println(ps.status, BIN);
     }
     if ( error_level == 1 ) {
       // chip in command mode
       // no clue how to end up here
-      Serial.print(F("warn command mode "));
+      Serial.print("warn command mode ");
       Serial.println(ps.status, BIN);
     }
     ps_convert(ps, &psi, &therm, OUTPUT_MIN, OUTPUT_MAX, PRESSURE_MIN, PRESSURE_MAX);
     // The sensor seems to always provide absolute pressure readings. Compensate by subtracting one atmosphere of pressure (14.695PSI)
     float gpsi = psi - 14.7;
-    char p_str[10];
-    dtostrf(gpsi, 2, 2, p_str);
-    Serial.print(F("Current PSI: "));
-    Serial.println(p_str);
+    //char p_str[10];
+    //dtostr(gpsi, p_str, 2);
+    //Serial.print("Current PSI: ");
+    //Serial.println(p_str);
     return gpsi;
   }
 }
@@ -91,25 +90,26 @@ boolean safety_engaged() {
   // the state of the pin. As such I need to invert the status.
   /*
   boolean not_safe = digitalRead(SAFETY_INPUT);
-  Serial.print(F("Safety Pin Status: "));
-  Serial.println(not_safe, DEC);
-  if ( not_safe ) {
-    return false;
-  } else {
-    return true;
-  }
-  */
+   Serial.print("Safety Pin Status: ");
+   Serial.println(not_safe, DEC);
+   if ( not_safe ) {
+   return false;
+   } else {
+   return true;
+   }
+   */
   boolean safety = digitalRead(SAFETY_INPUT);
-  Serial.print(F("Safety Pin Status: "));
+  Serial.print("Safety Pin Status: ");
   Serial.println(safety, DEC);
   return safety;
 }
 
 void energize_pump(boolean power) {
-  Serial.print(F("Pump Relay Status: "));
+  Serial.print("Pump Relay Status: ");
   Serial.println(power, DEC);
   digitalWrite(PUMP_RELAY, power);
 }
+
 
 void loop() {  
   if ( digitalRead(TRIGGER_INPUT) ) {
@@ -119,9 +119,11 @@ void loop() {
   // Turn the pump on when the storage tank pressure drops below the defined PSI.
   if ( cpsi < MIN_TANK_PSI ) {
     energize_pump(true);
-  // Turn the pump off when the pump meets or exceeds the defined PSI.
-  } else if ( cpsi >= MAX_TANK_PSI ) {
+    // Turn the pump off when the pump meets or exceeds the defined PSI.
+  } 
+  else if ( cpsi >= MAX_TANK_PSI ) {
     energize_pump(false);
   }
   delay(1000);
 }  
+
