4600abd58b225d19d0ebc85fba996f5662ad57c4
Ryan Hass <ryan@invalidchecksum.net>
2013-10-06 17:18:42 -0700

    Core funtionality implemented.
    Refactored yet again for Arduino due to complications with the·
    Energia/MSP430. Added full support for the NHD-C0220BiZ-FS(RGB)-FBW-3VM
    LCD. Note, there are a few hacks in the code which were done to
    get things going which need to be cleaned up.

--- bfg9000.ino
+++ bfg9000.ino
@@ -1,12 +1,13 @@
 // i2c library.
 #include <Wire.h>
-// LCD Libraries
-#include <ST7036.h>
-#include <LCD_C0220BiZ.h>
-#include <lcd.h>
-#include <LCD.h>
 // Honeywell SSC/HSC i2c pressure sensory library.
 #include <hsc_ssc_i2c.h>
+// LCD Libraries -- Not yet implemented
+#include <ST7036.h>
+#include <LCD_C0220BiZ.h>
+
+// Set the LCD type, configuration, and i2c address.
+ST7036 lcd = ST7036 ( 2, 20, 0x78 );
 
 // see hsc_ssc_i2c.h for a description of these values
 // these defaults are valid for the SSCDANN150PG2A3 chip
@@ -17,22 +18,29 @@
 #define PRESSURE_MAX 150   // 150psi
 
 // Application/implementation specific pin configuration.
-//#define ONBOARD_LED   P1_0
-#define TRIGGER_INPUT P1_3
-#define TRIGGER_RELAY P1_4
-#define SAFETY_INPUT  P2_6
-#define PUMP_RELAY    P1_1
+#define ONBOARD_LED   13
+#define TRIGGER_INPUT 10
+#define SAFETY_INPUT  2
+#define TRIGGER_RELAY 8
+#define PUMP_RELAY    12
 // Tank pressure settings -- the pump cannot output more than 20 PSI.
 #define MIN_TANK_PSI  10.0
 #define MAX_TANK_PSI  20.0
 
 void setup() {
-  //pinMode(ONBOARD_LED, OUTPUT);
-  pinMode(TRIGGER_INPUT, INPUT_PULLUP);
-  pinMode(TRIGGER_RELAY, OUTPUT);
+  pinMode(ONBOARD_LED, OUTPUT);
+  pinMode(TRIGGER_INPUT, INPUT);
   pinMode(SAFETY_INPUT, INPUT);
   pinMode(PUMP_RELAY, OUTPUT);
-  Serial.begin(9600);
+  pinMode(TRIGGER_RELAY, OUTPUT);
+  
+  digitalWrite(PUMP_RELAY, LOW);
+  digitalWrite(TRIGGER_RELAY, LOW);
+  
+  lcd.init();
+  lcd.setContrast(10);
+  
+  Serial.begin(115200);
 }
 
 void fire() {
@@ -41,9 +49,8 @@ void fire() {
     digitalWrite(TRIGGER_RELAY, HIGH);
     delay(25);
     digitalWrite(TRIGGER_RELAY, LOW);
-  } 
-  else {
-    Serial.println("Safety is engadged.");
+  } else {
+    Serial.println("Safety Engaged....");
   }
 }
 
@@ -52,52 +59,52 @@ float currentPsi() {
   uint8_t error_level = ps_get_raw(SLAVE_ADDR, &ps);
   float psi, therm;
   if ( error_level == 4 ) {
-    Serial.println("err pressure sensor missing");
+    Serial.println(F("err pressure sensor missing"));
   } 
   else {
     if ( error_level == 3 ) {
-      Serial.print("err pressure sensor diagnostic fault ");
+      Serial.print(F("err pressure sensor diagnostic fault "));
       Serial.println(ps.status, BIN);
     }
     if ( error_level == 2 ) {
       // if data has already been feched since the last
       // measurement cycle
-      Serial.print("warn stale pressure data ");
+      Serial.print(F("warn stale pressure data "));
       Serial.println(ps.status, BIN);
     }
     if ( error_level == 1 ) {
       // chip in command mode
       // no clue how to end up here
-      Serial.print("warn command mode ");
+      Serial.print(F("warn command mode "));
       Serial.println(ps.status, BIN);
     }
     ps_convert(ps, &psi, &therm, OUTPUT_MIN, OUTPUT_MAX, PRESSURE_MIN, PRESSURE_MAX);
-    // The sensor seems to always provide absolute pressure readings. Compensate by subtracting one atmosphere of pressure (14.695PSI)
+    // The sensor seems to always provide absolute pressure readings despite being a gage sensor. 
+    // Hack a compensation by subtracting one atmosphere of pressure (14.695PSI)
     float gpsi = psi - 14.7;
-    //char p_str[10];
-    //dtostr(gpsi, p_str, 2);
-    //Serial.print("Current PSI: ");
-    //Serial.println(p_str);
+    char p_str[10];
+    dtostrf(gpsi, 2, 2, p_str);
+    Serial.print("Current PSI: ");
+    Serial.println(p_str);
     return gpsi;
   }
 }
 
 
 boolean safety_engaged() {
-  delay(50); // Effective, but sloppy way to debounce the safety toggle switch.
   // The SPST switched used in my project has some odd characteristics which
   // prevent me from being able to use this as an INPUT_PULLUP pin which returns
   // the state of the pin. As such I need to invert the status.
-  /*
   boolean not_safe = digitalRead(SAFETY_INPUT);
-   Serial.print("Safety Pin Status: ");
-   Serial.println(not_safe, DEC);
-   if ( not_safe ) {
-   return false;
-   } else {
-   return true;
-   }
-   */
+  //Serial.print("Safety Pin Status: ");
+  //Serial.println(not_safe, DEC);
+  if (not_safe) {
+    digitalWrite(ONBOARD_LED, LOW);
+    return false;
+  } else {
+    digitalWrite(ONBOARD_LED, HIGH);
+    return true;
+  }
   boolean safety = digitalRead(SAFETY_INPUT);
   Serial.print("Safety Pin Status: ");
   Serial.println(safety, DEC);
@@ -110,20 +117,43 @@ void energize_pump(boolean power) {
   digitalWrite(PUMP_RELAY, power);
 }
 
-
-void loop() {  
-  if ( digitalRead(TRIGGER_INPUT) ) {
+void loop() {
+  boolean t = digitalRead(TRIGGER_INPUT);
+  if ( t == false ) {
     fire(); // Fire button pressed.
   }
+  lcd.clear();
+  
+  // Update the LCD with the current safety switch status.
+  if (safety_engaged()) {
+    lcd.setCursor(0,1);
+    lcd.print("====== SAFE ======");
+  } else {
+    lcd.setCursor(0,0);
+    lcd.print("******* ARMED *******");
+  }
+ 
+  // Store the current PSI for use in the loop.
   float cpsi = currentPsi();
+  
+  lcd.setCursor(1,0);
+  lcd.print("PSI: ");
+  lcd.setCursor(1,6); 
+  lcd.print(cpsi);
+   
+  lcd.setCursor(1,12);
+  lcd.print("Pump:");
   // Turn the pump on when the storage tank pressure drops below the defined PSI.
   if ( cpsi < MIN_TANK_PSI ) {
-    energize_pump(true);
+    energize_pump(true);    
+    lcd.print("ON");
     // Turn the pump off when the pump meets or exceeds the defined PSI.
   } 
   else if ( cpsi >= MAX_TANK_PSI ) {
     energize_pump(false);
+    lcd.print("OFF");
   }
-  delay(1000);
+ 
+  delay(500);
 }  
 
